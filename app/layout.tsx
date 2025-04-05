352a4e92a84686d8656d658e530fa8d37b4e9fa4
sahlct <mhdsahlct@gmail.com>
2025-04-05 16:34:40 +0530

    favicon changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <link rel="shortcut icon" href="./images/vmc_logo_01.png" type="image/x-icon" />
       <body className={inter.className}>
         <Header />
         {children}
